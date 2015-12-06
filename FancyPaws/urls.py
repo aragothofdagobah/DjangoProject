e71e5c60f8a0b476bb6b6d90bc11dabca0387547
Caleb <aragothofdagobah@gmail.com>
2015-12-06 17:12:24 -0500

    Fixed URLs

--- FancyPaws/urls.py
+++ FancyPaws/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^$', 'puppys.views.index'),
+    url(r'^contact/', 'puppys.views.contact'),
+    url(r'^about/', 'puppys.views.about'),
+    url(r'^ShihTzu/', 'puppys.views.shihTzu'),
+    url(r'^ToyPoodle/', 'puppys.views.toyPoodle'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- puppys/views.py
+++ puppys/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return render_to_response('puppys/index.html')
+    template = loader.get_template('puppys/index.html')
+    return HttpResponse(template.render())
+
+
+def contact(request):
+    template = loader.get_template('puppys/Contact.html')
+    return HttpResponse(template.render())
+
+
+def about(request):
+    template = loader.get_template('puppys/About.html')
+    return HttpResponse(template.render())
+
+
+def shihTzu(request):
+    template = loader.get_template('puppys/shihTzu.html')
+    return HttpResponse(template.render())
+
+
+def toyPoodle(request):
+    template = loader.get_template('puppys/toyPoodle.html')
+    return HttpResponse(template.render())
